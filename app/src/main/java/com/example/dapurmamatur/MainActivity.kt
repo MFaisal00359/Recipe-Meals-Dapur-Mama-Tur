fa5eeb99213381b9a8a46db9fef51bec14e1a823
Muhammad Faisal <mfaisal00359@gmail.com>
2024-05-08 21:45:49 +0700

    add: splash screen

--- app/src/main/java/com/example/dapurmamatur/MainActivity.kt
+++ app/src/main/java/com/example/dapurmamatur/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dapurmamatur
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val rootView = findViewById<View>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
---
+++ app/src/main/java/com/example/dapurmamatur/Register.java
@@ -0,0 +1,6 @@
+package com.example.dapurmamatur;
+
+import android.app.Activity;
+
+public class Register extends Activity {
+}
