73a263afb9bbabf47557da1f214d3efe25986081
Muhammad Faisal <mfaisal00359@gmail.com>
2024-05-18 16:05:30 +0700

    up

--- app/src/main/java/com/example/dapurmamatur/ui/view/DetailRecipeActivity.kt
+++ app/src/main/java/com/example/dapurmamatur/ui/view/DetailRecipeActivity.kt
@@ -1,10 +1,8 @@
 package com.example.dapurmamatur.ui.view
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dapurmamatur.R
--- app/src/main/java/com/example/dapurmamatur/utils/Constant.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dapurmamatur.utils
-
-object Constant {
-    const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
-    const val NETWORK_TIMEOUT = 60L
-}
